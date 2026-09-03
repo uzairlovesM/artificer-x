f54c327ba5660cfa62826d72e4c5afb5f6bd643a
Uzair <uzairlovesM@users.noreply.github.com>
2026-09-03 08:01:41 +0500

    feat:hsus7eush

--- app/src/main/java/com/waheed/artificerx/ui/screens/canvas/TimelapseScreen.kt
+++ app/src/main/java/com/waheed/artificerx/ui/screens/canvas/TimelapseScreen.kt
@@ -30,7 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.asImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 import com.waheed.artificerx.ui.theme.GoldPrimary
 import java.io.File
